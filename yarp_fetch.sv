c8576820be41b0fd0cd75c28b85532ec7f63f7f0
Dhirajzen <117260766+Dhirajzen@users.noreply.github.com>
2025-01-23 18:52:47 +0000

    Fetch Code Update

--- yarp_fetch.sv
+++ yarp_fetch.sv
@@ -8,12 +8,12 @@ module yarp_instr_mem (
 
   input    logic [31:0]   instr_mem_pc_i,
 
+  // Read data from memory
+  input    logic [31:0]   mem_rd_data_i,
+
   // Output read request to memory
   output   logic          instr_mem_req_o,
   output   logic [31:0]   instr_mem_addr_o,
-
-  // Read data from memory
-  input    logic [31:0]   mem_rd_data_i,
 
   // Instruction output
   output   logic [31:0]   instr_mem_instr_o
